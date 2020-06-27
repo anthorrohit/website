0a25173b4322c5c579b92a3858a68f3f7b6c55dd
anthorrohit <anthorrohit@gmail.com>
2020-06-27 10:49:36 +0530

    efg

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php 
-$conn= new mysqli("localhost","root","","cart_system");
+$conn= new mysqli("
+fdb22.atspace.me","
+3489782_3489782","mummy333@","3489782_3489782");
 if ($conn->connect_error) {
 	 die("f.$conn->connect_error");
 }
